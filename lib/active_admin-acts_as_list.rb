6ef3b191f336c47edcb66b3a63035f0345081469
Matt Brewer <matt.brewer@me.com>
2012-10-18 14:51:52 -0400

    Refactored so the gem actually worked with ActiveAdmin 0.4+

--- lib/active_admin-acts_as_list.rb
+++ lib/active_admin-acts_as_list.rb
@@ -1,6 +1,10 @@
 require 'active_admin/acts_as_list/engine'
 require 'active_admin/acts_as_list/version'
-require 'active_admin/acts_as_list/helper'
+require 'active_admin/acts_as_list/dsl'
+require 'active_admin/acts_as_list/index_methods'
 
 module ActiveAdminActsAsList
 end
+
+::ActiveAdmin::DSL.send(:include, ActiveAdmin::ActsAsList::DSL)
+::ActiveAdmin::Views::IndexAsTable.send(:include, ActiveAdmin::ActsAsList::IndexMethods)
