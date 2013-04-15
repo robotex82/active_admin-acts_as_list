98f55ade4c339bf719eb12bf00c7d534c6b91278
Roberto Vasquez Angel <robotex@robotex.de>
2013-04-15 11:29:05 +0200

    Fixed: Helper not being found in show views.

--- lib/active_admin-acts_as_list.rb
+++ lib/active_admin-acts_as_list.rb
@@ -1,6 +1,12 @@
+require 'active_admin'
+
 require 'active_admin/acts_as_list/engine'
 require 'active_admin/acts_as_list/version'
 require 'active_admin/acts_as_list/helper'
 
 module ActiveAdminActsAsList
 end
+
+ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::ActsAsList::Helper
+ActiveAdmin::Views::IndexAsTable.send :include, ActiveAdmin::ActsAsList::Helper
+ActiveAdmin::Views::Pages::Show.send :include, ActiveAdmin::ActsAsList::Helper
